543f7f3dd5756347f0883fa359c106cee9131b67
federicoalaniz <fedealaniz@gmail.com>
2017-10-19 20:47:16 -0300

    mod Inhalambrico

--- practico43/src/practico43/entidades/Inhalambrico.java
+++ practico43/src/practico43/entidades/Inhalambrico.java
@@ -13,14 +13,28 @@ public class Inhalambrico extends Telefono
 {
     private int canal;
     private double alcance;
-    
-    public int getCanal()
-    {
+
+    public int getCanal() {
         return canal;
     }
-    
-    public void setCanal(int canal)
-    {
-        
+
+    public void setCanal(int canal) {
+        this.canal = canal;
     }
+
+    public double getAlcance() {
+        return alcance;
+    }
+
+    public void setAlcance(double alcance) {
+        this.alcance = alcance;
+    }
+
+    public Inhalambrico(int canal, double alcance, long numero, String modelo, String marca) {
+        super(numero, modelo, marca);
+        this.canal = canal;
+        this.alcance = alcance;
+    }
+    
+    
 }
