1ec5354031d4fea0c3535389e6ceeead4a55e1d6
Usuario <Usuario@10.62.200.108>
2017-10-19 20:22:51 -0300

    cambios en persona

--- practico43/src/practico43/entidades/Persona.java
+++ practico43/src/practico43/entidades/Persona.java
@@ -9,6 +9,39 @@ package practico43.entidades;
  *
  * @author Federico Alaniz
  */
-public class Persona {
+public class Persona 
+{
+    private long dni;
+    private String nombre;
     
+    public long getDni()
+    {
+        return dni;
+    }
+    
+    public void setDni(long dni)
+    {
+        this.dni=dni;
+    }
+    
+    public String getNombre()
+    {
+        return nombre;
+    }
+    
+    public  void setNombre(String nombre)
+    {
+        this.nombre=nombre;
+    }
+    
+    public Persona(long dni,String nombre)
+    {
+        this.dni=dni;
+        this.nombre=nombre;
+    }
+    
+    public void llamarMensajear(Telefono tel,long nroDestinatario, String mensaje)
+    {
+        
+    }
 }
