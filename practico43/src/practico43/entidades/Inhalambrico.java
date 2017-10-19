92d4454980c490d5a52fa0326b316e8d41824222
Usuario <Usuario@10.62.200.108>
2017-10-19 20:45:14 -0300

    creacion de clase inhalambrico

---
+++ practico43/src/practico43/entidades/Inhalambrico.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practico43.entidades;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Inhalambrico extends Telefono 
+{
+    private int canal;
+    private double alcance;
+    
+    public int getCanal()
+    {
+        return canal;
+    }
+    
+    public void setCanal(int canal)
+    {
+        
+    }
+}
