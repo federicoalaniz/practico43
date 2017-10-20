413b09fbc32de3247caad91d9258e2d7c42b2e62
Usuario <Usuario@10.62.200.108>
2017-10-20 15:34:24 -0300

    pruba de la clase test

--- practico43/src/practico43/test/TestTelefono.java
+++ practico43/src/practico43/test/TestTelefono.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package practico43.test;
+import practico43.entidades.*;
 
 /**
  *
@@ -13,6 +14,9 @@ public class TestTelefono
 {
     public static void main(String[] args)
     {
-        
+        Telefono cel = new Fijo("Domestica", 437138,"2hj4","Samsung");
+        Persona p=new Persona(39992375,"Romina");
+        p.llamarMensajear(cel, 15557892);
+        p.llamarMensajear(cel, 15851526, "Hola. Que tal?");
     }
 }
