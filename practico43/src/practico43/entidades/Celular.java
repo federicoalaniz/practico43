78da63b1e41344b305330fdb24683e33f0560264
federicoalaniz <fedealaniz@gmail.com>
2017-10-20 14:52:29 -0300

    mod clase Celular

---
+++ practico43/src/practico43/entidades/Celular.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practico43.entidades;
+
+/**
+ *
+ * @author Federico Alaniz
+ */
+public class Celular extends Telefono{
+    private String empresa;
+
+    public Celular(String empresa, long numero, String modelo, String marca) {
+        super(numero, modelo, marca);
+        this.empresa = empresa;
+    }
+
+    public String getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(String empresa) {
+        this.empresa = empresa;
+    }
+    public boolean llamar(long nroDestino){
+        return true;
+    }
+}
